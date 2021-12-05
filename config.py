946495d2567ed78bc1241bbf5e78aaa51b7fab41
1enzi <lenzi.ira@gmail.com>
2021-12-05 07:36:34 +0300

    Обновила название игры
    Так как старая была криво настроена, а изменить её не смогла :disappointed_relieved:

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ TESTTOKEN = '5022186293:AAGJ1-c6F50Nxzo-gKmjOsk6bKTGgsSsOjQ'
 DBURL = 'mongodb://admin:pass@cluster0-shard-00-00.2tl16.mongodb.net:27017,cluster0-shard-00-01.2tl16.mongodb.net:27017,cluster0-shard-00-02.2tl16.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-p4ukz5-shard-0&authSource=admin&retryWrites=true&w=majority'
 
 GAMEURL = 'https://darkenlord1.github.io/SecuTor/'
-GAMENAME = 'primary'
+GAMENAME = 'main_quiz'
